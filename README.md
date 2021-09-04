2b5c3e7998f25668c6b5dde9b0db5295b0d4f52a
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-09-03 22:19:58 -0300

    finished the software

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # pysheet
 Python program to work with online sheets from Microsoft Excel
 
+https://developers.google.com/sheets/api/quickstart/python
+
 ```bash
 $ pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 ```
--- pysheet.py
+++ pysheet.py
@@ -4,6 +4,7 @@
 from __future__ import print_function
 
 import os.path
+from math import ceil
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -15,7 +16,9 @@ SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = "1ZrG8DSN-_8MTj38JOrnfuY_iOdHh1G8irwWj-1f82Zk"
-SAMPLE_RANGE_NAME = "A4:E"
+SAMPLE_RANGE_NAME = "C4:G"
+UPDATE_RANGE = "G4:H"
+CLASSES_RANGE = "A2"
 
 
 def main():
@@ -53,10 +56,48 @@ def main():
     if not values:
         print("No data found.")
     else:
-        print("Name, Major:")
+        classes_result = (
+            sheet.values()
+            .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=CLASSES_RANGE)
+            .execute()
+        )
+        classes = int(classes_result.get("values", [])[0][0].split(": ")[1])
+
+        updated_values = list()
+
         for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print("%s, %s" % (row[0], row[4]))
+            # Average calc
+            average = ceil((int(row[1]) + int(row[2]) + int(row[3])) / 3)
+            score_needed = 0
+
+            if average < 50:
+                situation = "Reprovado por Nota"
+            elif average >= 70:
+                situation = "Aprovado"
+            else:
+                situation = "Exame Final"
+                score_needed = 100 - average
+
+            # class misses calc
+            if int(row[0]) / classes > 0.25:
+                situation = "Reprovado por Falta"
+                score_needed = 0
+
+            updated_values.append([situation, score_needed])
+
+        # Update the cells
+        body = {"values": updated_values}
+        result = (
+            sheet.values()
+            .update(
+                spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                range=UPDATE_RANGE,
+                valueInputOption="USER_ENTERED",
+                body=body,
+            )
+            .execute()
+        )
+        print("{0} cells updated.".format(result.get("updatedCells")))
 
 
 if __name__ == "__main__":
